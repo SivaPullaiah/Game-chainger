5fd1964c8030ea3ecdec111a16ee79e13475b365
siva <mangalisivapullaiah@gmail.com>
2023-04-19 20:08:30 +0530

    Revert "Update addition.py"
    This reverts commit e5517df0856dc611e1c6c1f6d6bbd377e677d611.

--- addition.py
+++ addition.py
@@ -3,6 +3,5 @@ num2 = int(input("Enter second number: "))
 num3 = int(input("Enter third number : "))
 # addition
 result = num1 + num2 +num3
-print("Hello")
 
 print("The sum of {0} and {1} is {2}".format(num1, num2, result))
